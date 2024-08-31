fcb60d10c84a9530974e93ad72145be40aaeb341
Susan Gautam <off.susangautam@gmail.com>
2024-08-31 11:27:04 +0545

    Create questions.js

---
+++ quiz-app/questions.js
@@ -0,0 +1,57 @@
+const questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        options: [
+            "HyperText Markup Language",
+            "HighText Markup Language",
+            "HyperText Multi Language",
+            "HighText Multi Language"
+        ],
+        answer: "HyperText Markup Language"
+    },
+    {
+        numb: 2,
+        question: "Which of the following is a JavaScript framework?",
+        options: [
+            "React",
+            "Laravel",
+            "Django",
+            "Flask"
+        ],
+        answer: "React"
+    },
+    {
+        numb: 3,
+        question: "Which method is used to access the first element of an array in JavaScript?",
+        options: [
+            "first()",
+            "get()",
+            "head()",
+            "shift()"
+        ],
+        answer: "shift()"
+    },
+    {
+        numb: 4,
+        question: "Which company developed JavaScript?",
+        options: [
+            "Netscape",
+            "Microsoft",
+            "Google",
+            "Apple"
+        ],
+        answer: "Netscape"
+    },
+    {
+        numb: 5,
+        question: "Which operator is used to assign a value to a variable in JavaScript?",
+        options: [
+            "=",
+            "==",
+            "===",
+            "=>"
+        ],
+        answer: "="
+    }
+];
